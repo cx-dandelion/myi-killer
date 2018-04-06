5a49394934c67b94a6903b5885434324a92d1114
chainsx(cx-dandelion) <chainsx@users.noreply.github.com>
2018-04-07 01:35:35 +0800

    Create time.java

---
+++ Unlock/time.java
@@ -0,0 +1,9 @@
+import java.util.Date; 
+import java.text.SimpleDateFormat;
+
+public class NowString { 
+   public static void main(String[] args) {      
+   SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH");//设置日期格式
+      System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+   } 
+}
